6563352f2290a9ece18542b84c00b79c3c1f43f0
chextopher <christopherjideofor@gmail.com>
2022-05-19 02:35:57 -0700

    third commit of Simple.java to reflect updated java code

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
 class Simple{
-    public static void public static void main(String[] args) {
-        system.out.println("Hello Chex, Welcome to Git Online Training!!");
+    public static void main(String args[]) {
+    System.out.println("Hello Chex, Welcome to Git Online Training!!");
    }
 }
